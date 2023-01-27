40969608854fa61a9e8169f65236ac1fc393a19e
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-01-27 17:33:44 +0600

    cors error

--- backend/admin-server/dist/src/routers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testRouter = void 0;
-var test_router_1 = require("./test.router");
-Object.defineProperty(exports, "testRouter", { enumerable: true, get: function () { return __importDefault(test_router_1).default; } });
--- backend/admin-server/dist/src/services/test.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function test() {
-    return 'test service working';
-}
-const testSqaureFunction = async (num) => {
-    return num * num;
-};
-exports.default = { test, testSqaureFunction };
