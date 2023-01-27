458d5a5afc9b828892aa758e4ae55fa2875005ea
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-01-27 07:07:51 +0600

    documentation added

--- documentation/note.md
+++ documentation/note.md
@@ -2,14 +2,8 @@ This is documentation for project
 
 //TODO
 -[] Use case diagram
+-[] Swim lane diagram
 -[] Block diagram/Application architecture
 -[] Data flow diagram
 -[] Entity relation diagram
 -[] State diagram
-
-Link of the docs
-https://docs.google.com/document/d/1057zojrSR9_rK9akkWc3CNL_75TyYAql0c3Z4f1m88Y/edit?usp=sharing
-
-Link of the figma file
-https://www.figma.com/file/ezbyOA7JqmbHA2RUFpzmGA/Sust-Hackathon-23-Team-Pixel?node-id=0%3A1&t=ti53q5JgnwImfp6q-1
-
---
+++ frontend/frontend-client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/authPages/Login";
+import Register from "./pages/authPages/Register";
+import HomePage from "./pages/HomePage";
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
