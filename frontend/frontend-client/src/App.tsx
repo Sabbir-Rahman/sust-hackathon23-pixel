ca963a65be175e5e24c7aff824b36a9acc2a88e3
Ibtid <ibtid011@example.com>
2023-01-27 14:53:07 +0600

    subscription page

--- frontend/frontend-client/src/App.tsx
+++ frontend/frontend-client/src/App.tsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import Heatmap from "./component/Analytics/heatmap.component";
-import Login from "./pages/authPages/Login";
-import Register from "./pages/authPages/Register";
-import HomePage from "./pages/HomePage";
+import { Route, Routes } from 'react-router-dom';
+import Heatmap from './component/Analytics/heatmap.component';
+import Login from './pages/authPages/Login';
+import Register from './pages/authPages/Register';
+import HomePage from './pages/HomePage';
+import SubscriptionPage from './pages/SubscriptionPage';
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/subscription-page' element={<SubscriptionPage />} />
       </Routes>
     </div>
   );
