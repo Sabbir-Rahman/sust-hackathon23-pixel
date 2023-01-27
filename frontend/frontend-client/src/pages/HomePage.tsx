5155c410b1737992a2d6a855455edbe5ab3e7e3a
JaberHPranto <jabearhossain@iut-dhaka.edu>
2023-01-27 09:37:24 +0600

    homepage

--- frontend/frontend-client/src/pages/HomePage.tsx
+++ frontend/frontend-client/src/pages/HomePage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>This is Home Page</div>;
+  return (
+    <div className="w-full bg-bgColor flex">
+      {/* Post Problem */}
+      <div className="bg-red-400 w-1/4">1</div>
+      {/* Newsfeed */}
+      <div className="bg-purple-400 w-2/4">2</div>
+      {/* Home Navigation */}
+      <div className="bg-green-400 w-1/4">3</div>
+    </div>
+  );
 };
 
 export default HomePage;
