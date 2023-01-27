ae35808bb4715313cdabbccc7b622f32303f664a
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-01-27 07:04:05 +0600

    initial backend server seup with mongodb,typescript and testing

---
+++ marketplace-server/src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import { testRouter } from './routers'
+import { logInfo } from '../logger/customLogger'
+import connect from './utils/mongodbConnect'
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Welcome')
+})
+
+app.use('/api',testRouter)
+
+app.listen(port, async () => {
+  logInfo('index.ts', './index.ts', `Server running on ${port}`)
+  await connect()
+})
