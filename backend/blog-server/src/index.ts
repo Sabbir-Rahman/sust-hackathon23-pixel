24d7c507480c514cf46c656e6d5d93c8882f87ca
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-01-27 17:25:18 +0600

    cors error solved

--- backend/blog-server/src/index.ts
+++ backend/blog-server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { postRouter, testRouter } from './routers'
 import { logInfo } from '../logger/customLogger'
 import connect from './utils/mongodbConnect'
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(
   express.urlencoded({
